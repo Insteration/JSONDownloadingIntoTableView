6115190fe18fbfcea855e562f4e84464e5d85f4d
Artem <inste@icloud.com>
2019-05-30 21:29:11 +0300

    Second commit

---
+++ JSONDownloadingIntoTableView/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  JSONDownloadingIntoTableView
+//
+//  Created by Артём Кармазь on 5/30/19.
+//  Copyright © 2019 Art Karma. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
